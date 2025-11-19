a81e9e59663425d91e837d0d7f77a3c7acbad9f5
Myat Zaw <myatzaw@gicjp.com>
2025-11-19 13:44:48 +0630

    Update NovDay2.sql
    Added Q4,Q5. Changed Q8.

--- Database/DB_NovDay2/NovDay2.sql
+++ Database/DB_NovDay2/NovDay2.sql
@@ -37,6 +37,41 @@ GROUP BY customer_id;
 -----------Q4-----------
 SELECT * FROM EMPLOYEES;
 
+--A
+SELECT MAX(salary) FROM EMPLOYEES WHERE salary < (SELECT MAX(salary)
+FROM EMPLOYEES);
+
+--B
+SELECT salary FROM EMPLOYEES ORDER BY salary DESC LIMIT 1,1;
+
+--c
+SELECT MIN(salary) FROM (SELECT DISTINCT salary FROM EMPLOYEES
+ORDER BY salary DESC) WHERE ROWNUM <= 2;
+
+--D
+SELECT salary FROM (SELECT salary, DENSE_RANK() OVER (ORDER BY salary
+DESC) AS rank FROM EMPLOYEES) WHERE rank = 2;
+
+-----------Q5-----------
+SELECT * FROM sales;
+
+--A
+SELECT product_id, SUM(AMOUNT) FROM sales GROUP BY product_id HAVING
+SUM(AMOUNT) > AVG(AMOUNT);
+
+--B
+SELECT product_id, SUM(AMOUNT) FROM sales GROUP BY product_id HAVING
+SUM(AMOUNT) > (SELECT AVG(total_sales) FROM (SELECT SUM(AMOUNT) as
+total_sales FROM sales GROUP BY product_id));
+
+--C
+SELECT product_id, SUM(AMOUNT) FROM sales WHERE SUM(AMOUNT) > (SELECT
+AVG(AMOUNT) FROM sales) GROUP BY product_id;
+
+--D
+SELECT product_id, SUM(AMOUNT) FROM sales GROUP BY product_id WHERE
+SUM(AMOUNT) > AVG(SUM(AMOUNT));
+
 -----------Q6-----------
 SELECT * FROM SALES;
 SELECT product_id, SUM(AMOUNT)
@@ -80,6 +115,39 @@ customer_id, order_date WHERE order_date >= SYSDATE - 30;
 
 ----------Q8------------
 SELECT * FROM PRODUCTS;
+SELECT * FROM SALES;
+
+--A
 SELECT CATEGORY, PRODUCT_NAME, SUM(PRICE) AS total_sales
 FROM PRODUCTS
 GROUP BY CUBE(CATEGORY, PRODUCT_NAME);
+
+--B
+SELECT 
+    Category,
+    PRODUCT_NAME,
+    SUM(price) AS total_sales
+FROM products
+GROUP BY ROLLUP(Category, PRODUCT_NAME);
+
+--C
+SELECT 
+    Category,
+    PRODUCT_NAME,
+    SUM(price) AS total_sales
+FROM products
+GROUP BY GROUPING SETS (
+    (Category, PRODUCT_NAME),   
+    (Category),          
+    (PRODUCT_NAME),                     
+    ()                            
+);
+
+--D
+SELECT 
+    CATEGORY,
+    PRODUCT_NAME,
+    SUM(price) AS total_sales
+FROM products
+GROUP BY ALL CATEGORY, PRODUCT_NAME;
+
